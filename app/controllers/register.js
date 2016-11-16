c665c205f5e18b680d9a6bbe6cabfc66a24d942c
webwerks1 <webwerks1@Rahul-Mac-mini.local>
2016-11-16 18:10:57 +0530

    all set only DOB

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -25,19 +25,42 @@ function check_Register(e) {
   var passwordRE = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){6,16}/;
   if($.first_name.value=="")
   {
-
+    $.viewfirst_name.backgroundColor="blue";
     alert("please fill first name");
   }
   else if ($.last_name.value=="") {
+      $.viewfirst_name.backgroundColor="transprent";
+      $.viewlast_name.backgroundColor="blue";
     alert("please fill last name");
   }
   else if (emailRE.test($.email_id.value)==false) {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="blue";
     alert("Invalid email_id");
   }
   else if (passwordRE.test($.password.value)==false) {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="blue";
     alert("Password must be 1 lower 1 upper and 1 digit");
   }
+  else if ($.password.value!=$.conform_password.value) {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="blue";
+    alert("Password is not matched");
+  }
   else if (mobRE.test($.phone_no.value)==false) {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="transprent";
+      $.viewphone_no.backgroundColor="blue";
     alert("Phone No must be 10 digits");
 
   }
@@ -46,31 +69,103 @@ function check_Register(e) {
     alert("please fill valid Date of Birth");
   }
 
+else if ($.male.text=="\uf1db" && $.female.text=="\uf1db") {
+  $.viewfirst_name.backgroundColor="transprent";
+  $.viewlast_name.backgroundColor="transprent";
+    $.viewemail_id.backgroundColor="transprent";
+    $.viewpassword.backgroundColor="transprent";
+    $.viewconform_password.backgroundColor="transprent";
+    $.viewphone_no.backgroundColor="transprent";
+    $.viewGender.backgroundColor="blue";
+   alert("please Select Gender");
+
+}
   else if ($.country.value=="") {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="transprent";
+      $.viewphone_no.backgroundColor="transprent";
+      $.viewGender.backgroundColor="transprent";
+      $.viewcountry.backgroundColor="blue";
      alert("please fill country");
 
   }
   else if ($.state.value=="") {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="transprent";
+      $.viewphone_no.backgroundColor="transprent";
+      $.viewGender.backgroundColor="transprent";
+      $.viewcountry.backgroundColor="transprent";
+      $.viewstate.backgroundColor="blue";
     alert("please fill state");
   }
   else if ($.city.value=="") {
-    Ti.API.info("city");
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="transprent";
+      $.viewphone_no.backgroundColor="transprent";
+      $.viewGender.backgroundColor="transprent";
+      $.viewcountry.backgroundColor="transprent";
+      $.viewstate.backgroundColor="transprent";
+      $.viewcity.backgroundColor="blue";
     alert("please fill city");
   }
   else if (zipRE.test($.zipcode.value)==false) {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="transprent";
+      $.viewphone_no.backgroundColor="transprent";
+      $.viewGender.backgroundColor="transprent";
+      $.viewcountry.backgroundColor="transprent";
+      $.viewstate.backgroundColor="transprent";
+      $.viewcity.backgroundColor="transprent";
+      $.viewzipcode.backgroundColor="blue";
     alert("zipcode must be 6 digits");
-
   }
   else if ($.add1.value=="") {
-    Ti.API.info("add1");
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="transprent";
+      $.viewphone_no.backgroundColor="transprent";
+      $.viewGender.backgroundColor="transprent";
+      $.viewcountry.backgroundColor="transprent";
+      $.viewstate.backgroundColor="transprent";
+      $.viewcity.backgroundColor="transprent";
+      $.viewzipcode.backgroundColor="transprent";
+      $.viewadd1.backgroundColor="blue";
     alert("please fill add1");
   }
-  else if ($.add2.value=="") {
-    Ti.API.info("add2");
-    alert("please fill add2");
+
+  else if ($.check.text=="\uf0c8") {
+    $.viewfirst_name.backgroundColor="transprent";
+    $.viewlast_name.backgroundColor="transprent";
+      $.viewemail_id.backgroundColor="transprent";
+      $.viewpassword.backgroundColor="transprent";
+      $.viewconform_password.backgroundColor="transprent";
+      $.viewphone_no.backgroundColor="transprent";
+      $.viewGender.backgroundColor="transprent";
+      $.viewcountry.backgroundColor="transprent";
+      $.viewstate.backgroundColor="transprent";
+      $.viewcity.backgroundColor="transprent";
+      $.viewzipcode.backgroundColor="transprent";
+      $.viewadd1.backgroundColor="transprent";
+      $.viewcheck.backgroundColor="blue";
+    alert("please check out Terms and conditions");
   }
-    Ti.API.info("by");
-    alert("byyy");
+
+    $.viewcheck.backgroundColor="transprent";
+    alert("Congradulations Your Registration is Successful !");
   }
 
 
@@ -167,21 +262,49 @@ var opts={
 
   ]
 }
+function BirthdayPicker() {
+  Ti.UI.backgroundColor = 'white';
+var win = Ti.UI.createWindow({
+  exitOnClose: true,
+  layout: 'vertical'
+});
 
+var picker = Ti.UI.createPicker({
+  type:Ti.UI.PICKER_TYPE_DATE,
+  minDate:new Date(2009,0,1),
+  maxDate:new Date(2014,11,31),
+  value:new Date(2014,3,12),
+  top:50
+});
+
+win.add(picker);
+win.open();
+
+picker.addEventListener('change',function(e){
+  Ti.API.info("User selected date: " + e.value.toLocaleString());
+});
+}
      function dropdowncountry(e)
      {
-
-            $.countrydialog.setOptions([opts.Countries[0].Name,opts.Countries[1].Name,opts.Countries[2].Name]);
+            Ti.API.info("rrrrr");
+            $.state.text="Select State";
+            $.city.text="Select City";
+            $.countrydialog.setOptions([opts.Countries[0].Name,
+              opts.Countries[1].Name,
+              opts.Countries[2].Name,"Cancel"]);
             $.countrydialog.show();
             $.countrydialog.addEventListener('click',function(e){
                     if (e.index==0) {
-                        $.country.value="USA";
+                        $.country.text="USA";
                     }
                     else if (e.index==1) {
-                        $.country.value="INDIA";
+                        $.country.text="INDIA";
+                    }
+                    else if(e.index==2) {
+                      $.country.text="AUS";
                     }
                     else {
-                      $.country.value="AUS";
+                        $.country.text="Select Country"
                     }
 
               });
@@ -190,158 +313,209 @@ var opts={
 
     function dropdownstate(e)
     {
-      if ($.country.value=="") {
+      $.city.text="Select City";
+      if ($.country.text=="Select Country") {
            alert("please fill country First");
          }
-        else    if ($.country.value=="INDIA") {
+        else    if ($.country.text=="INDIA") {
                       $.statedialog.setOptions([
                                     opts.Countries[1].States[0].Name,
                                     opts.Countries[1].States[1].Name,
-                                    opts.Countries[1].States[2].Name
+                                    opts.Countries[1].States[2].Name,"Cancel"
                                   ]);
                         $.statedialog.show();
                         $.statedialog.addEventListener('click',function(e)
                         {
-                                $.state.value=e.source.options[e.index];
+                          if (e.index==3) {
+                            $.state.text="Select State"
+                          } else {
+                              $.state.text=e.source.options[e.index];
+                          }
              });
       }
-      else if ($.country.value=="USA")
+      else if ($.country.text=="USA")
         {
           $.statedialog.setOptions([opts.Countries[0].States[0].Name,
                         opts.Countries[0].States[1].Name,
-                        opts.Countries[0].States[2].Name
+                        opts.Countries[0].States[2].Name,"Cancel"
                       ]);
             $.statedialog.show();
             $.statedialog.addEventListener('click',function(e)
             {
-                    $.state.value=e.source.options[e.index];
+              if (e.index==3) {
+                $.state.text="Select State"
+              } else {
+                  $.state.text=e.source.options[e.index];
+              }
+
  });
   }
-  else if ($.country.value=="AUS")
+  else if ($.country.text=="AUS")
     {
       $.statedialog.setOptions([
                     opts.Countries[2].States[0].Name,
                     opts.Countries[2].States[1].Name,
-                    opts.Countries[2].States[2].Name
+                    opts.Countries[2].States[2].Name,"Cancel"
                   ]);
         $.statedialog.show();
         $.statedialog.addEventListener('click',function(e)
         {
-                $.state.value=e.source.options[e.index];
+          if (e.index==3) {
+            $.state.text="Select State"
+          } else {
+              $.state.text=e.source.options[e.index];
+          }
 });
 }
 
 }
 function dropdowncity() {
-  if ($.state.value=="") {
+
+  if ($.state.text=="Select State") {
        alert("please fill state First");
      }
-  if ($.state.value=="AUS-State1") {
+  if ($.state.text=="AUS-State1") {
             $.citydialog.setOptions([
                           opts.Countries[2].States[0].Cities[0],
                           opts.Countries[2].States[0].Cities[1],
-                          opts.Countries[2].States[0].Cities[2]
+                          opts.Countries[2].States[0].Cities[2],"Cancel"
                         ]);
               $.citydialog.show();
               $.citydialog.addEventListener('click',function(e)
               {
-                      $.city.value=e.source.options[e.index];
+                if (e.index==3) {
+                  $.city.text="Select City"
+                } else {
+                    $.city.text=e.source.options[e.index];
+                }
    });
 }
-else if ($.state.value=="AUS-State2") {
+else if ($.state.text=="AUS-State2") {
   $.citydialog.setOptions([
                 opts.Countries[2].States[1].Cities[0],
                 opts.Countries[2].States[1].Cities[1],
-                opts.Countries[2].States[1].Cities[2]
+                opts.Countries[2].States[1].Cities[2],"Cancel"
               ]);
     $.citydialog.show();
     $.citydialog.addEventListener('click',function(e)
     {
-            $.city.value=e.source.options[e.index];
+      if (e.index==3) {
+        $.city.text="Select City"
+      } else {
+          $.city.text=e.source.options[e.index];
+      }
 });
 }
-else if ($.state.value=="AUS-State3") {
+else if ($.state.text=="AUS-State3") {
   $.citydialog.setOptions([
                 opts.Countries[2].States[2].Cities[0],
                 opts.Countries[2].States[2].Cities[1],
-                opts.Countries[2].States[2].Cities[2]
+                opts.Countries[2].States[2].Cities[2],"Cancel"
               ]);
     $.citydialog.show();
     $.citydialog.addEventListener('click',function(e)
     {
-            $.city.value=e.source.options[e.index];
+      if (e.index==3) {
+        $.city.text="Select City"
+      } else {
+          $.city.text=e.source.options[e.index];
+      }
 });
 }
-if ($.state.value=="IND-State1") {
+if ($.state.text=="IND-State1") {
           $.citydialog.setOptions([
                         opts.Countries[1].States[0].Cities[0],
                         opts.Countries[1].States[0].Cities[1],
-                        opts.Countries[1].States[0].Cities[2]
+                        opts.Countries[1].States[0].Cities[2],"Cancel"
                       ]);
             $.citydialog.show();
             $.citydialog.addEventListener('click',function(e)
             {
-                    $.city.value=e.source.options[e.index];
+              if (e.index==3) {
+                $.city.text="Select City"
+              } else {
+                  $.city.text=e.source.options[e.index];
+              }
  });
 }
-else if ($.state.value=="IND-State2") {
+else if ($.state.text=="IND-State2") {
 $.citydialog.setOptions([
               opts.Countries[1].States[1].Cities[0],
               opts.Countries[1].States[1].Cities[1],
-              opts.Countries[1].States[1].Cities[2]
+              opts.Countries[1].States[1].Cities[2],"Cancel"
             ]);
   $.citydialog.show();
   $.citydialog.addEventListener('click',function(e)
   {
-          $.city.value=e.source.options[e.index];
+    if (e.index==3) {
+      $.city.text="Select City"
+    } else {
+        $.city.text=e.source.options[e.index];
+    }
 });
 }
-else if ($.state.value=="IND-State3") {
+else if ($.state.text=="IND-State3") {
 $.citydialog.setOptions([
               opts.Countries[1].States[2].Cities[0],
               opts.Countries[1].States[2].Cities[1],
-              opts.Countries[1].States[2].Cities[2]
+              opts.Countries[1].States[2].Cities[2],"Cancel"
             ]);
   $.citydialog.show();
   $.citydialog.addEventListener('click',function(e)
   {
-          $.city.value=e.source.options[e.index];
+    if (e.index==3) {
+      $.city.text="Select City"
+    } else {
+        $.city.text=e.source.options[e.index];
+    }
 });
 }
-if ($.state.value=="USA-State1") {
+if ($.state.text=="USA-State1") {
           $.citydialog.setOptions([
                         opts.Countries[0].States[0].Cities[0],
                         opts.Countries[0].States[0].Cities[1],
-                        opts.Countries[0].States[0].Cities[2]
+                        opts.Countries[0].States[0].Cities[2],"Cancel"
                       ]);
             $.citydialog.show();
             $.citydialog.addEventListener('click',function(e)
             {
-                    $.city.value=e.source.options[e.index];
+              if (e.index==3) {
+                $.city.text="Select City"
+              } else {
+                  $.city.text=e.source.options[e.index];
+              }
  });
 }
-else if ($.state.value=="USA-State2") {
+else if ($.state.text=="USA-State2") {
 $.citydialog.setOptions([
               opts.Countries[0].States[1].Cities[0],
               opts.Countries[0].States[1].Cities[1],
-              opts.Countries[0].States[1].Cities[2]
+              opts.Countries[0].States[1].Cities[2],"Cancel"
             ]);
   $.citydialog.show();
   $.citydialog.addEventListener('click',function(e)
   {
-          $.city.value=e.source.options[e.index];
+    if (e.index==3) {
+      $.city.text="Select City"
+    } else {
+        $.city.text=e.source.options[e.index];
+    }
 });
 }
-else if ($.state.value=="USA-State3") {
+else if ($.state.text=="USA-State3") {
 $.citydialog.setOptions([
               opts.Countries[0].States[2].Cities[0],
               opts.Countries[0].States[2].Cities[1],
-              opts.Countries[0].States[2].Cities[2]
+              opts.Countries[0].States[2].Cities[2],"Cancel"
             ]);
   $.citydialog.show();
   $.citydialog.addEventListener('click',function(e)
   {
-          $.city.value=e.source.options[e.index];
+    if (e.index==3) {
+      $.city.text="Select City"
+    } else {
+        $.city.text=e.source.options[e.index];
+    }
 });
 }
 
@@ -352,6 +526,9 @@ function goto_back() {
 }
 function cleartext(e){
   e.source.value=" ";
+}
+function clearlabel(e){
+  e.source.text=" ";
 }
 function clickMale() {
   $.male.text="\uf111";
